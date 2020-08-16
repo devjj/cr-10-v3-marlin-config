656c2b270b8ef122ed87dd50f6c9107bd5d85816
Eric Schewe <eric@fizi.ca>
2020-08-15 19:01:23 -0700

    Further tweaking to probe location, actually measured this time instead of guessing

--- Configuration.h
+++ Configuration.h
@@ -1014,7 +1014,7 @@
  *     |    [-]    |
  *     O-- FRONT --+
  */
-#define NOZZLE_TO_PROBE_OFFSET { 47, 5, 0 }
+#define NOZZLE_TO_PROBE_OFFSET { 44, 7, 0 }
 
 // Most probes should stay away from the edges of the bed, but
 // with NOZZLE_AS_PROBE this can be negative for a wider probing area.
