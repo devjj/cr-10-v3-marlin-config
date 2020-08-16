da23f9ac7e803dbb94572626845b3a9befbede58
Eric Schewe <eric@fizi.ca>
2020-08-16 13:54:16 -0700

    Changed E Steps from 97mm/sec to 415mm/sec per tavella's helpful suggestion: https://www.reddit.com/r/CR10/comments/i8obod/marlin_2x_on_a_cr10_v3/g1r5n9z

--- Configuration.h
+++ Configuration.h
@@ -762,7 +762,7 @@
  * Override with M92
  *                                      X, Y, Z, E0 [, E1[, E2...]]
  */
-#define DEFAULT_AXIS_STEPS_PER_UNIT   { 80, 80, 400, 97 }
+#define DEFAULT_AXIS_STEPS_PER_UNIT   { 80, 80, 400, 415 }
 
 /**
  * Default Max Feed Rate (mm/s)
--- Version.h
+++ Version.h
@@ -41,7 +41,7 @@
  * here we define this default string as the date where the latest release
  * version was tagged.
  */
-#define STRING_DISTRIBUTION_DATE "2020-08-15 17:26"
+#define STRING_DISTRIBUTION_DATE "2020-08-16 13:40"
 
 /**
  * Defines a generic printer name to be output to the LCD after booting Marlin.
