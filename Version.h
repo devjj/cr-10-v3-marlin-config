f74be558e3ceade8ad4f7f2186e8b4555a17de19
Eric Schewe <eric@fizi.ca>
2020-08-19 12:22:57 -0700

    Forgot to update the version string

--- Version.h
+++ Version.h
@@ -41,7 +41,7 @@
  * here we define this default string as the date where the latest release
  * version was tagged.
  */
-#define STRING_DISTRIBUTION_DATE "2020-08-16 13:40"
+#define STRING_DISTRIBUTION_DATE "2020-08-19 12:22"
 
 /**
  * Defines a generic printer name to be output to the LCD after booting Marlin.
